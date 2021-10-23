5293e572f4ecae6726d9f0e103a4159f79d48f07
Risman Abdilah <risman.abdilah@frisidea.com>
2021-10-23 23:38:01 +0700

    Belajar Framework Lumen (Membuat REST API Blog) Bagian #1

---
+++ database/migrations/2021_10_23_154539_create_table_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string("email")->unique();
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("motto");
+            $table->string("bio");
+            $table->string("password");
+            $table->string("api_token");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
